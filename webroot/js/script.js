23be5f4e1dc72b59c95cf418cd83b61296864891
krisengstrom <kristofer.engstrom@gmail.com>
2015-12-09 13:25:01 +1100

    steelcase logo height

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -2,15 +2,18 @@
 
 	var buttons = document.querySelectorAll('button');
 	for (var i=0; i<buttons.length; i++) {
-		buttons[i].addEventListener('click', function() {
-			var scrollStep = -window.scrollY / (1000 / 15);
-			var scrollInterval = setInterval(function() {
-				if (window.scrollY != 0)
-					window.scrollBy(0, scrollStep);
-				else
-					clearInterval(scrollInterval);
-			}, 15);
-		});
+		
+		if (buttons[i].type == 'button') {
+			buttons[i].addEventListener('click', function() {
+				var scrollStep = -window.scrollY / (1000 / 15);
+				var scrollInterval = setInterval(function() {
+					if (window.scrollY != 0)
+						window.scrollBy(0, scrollStep);
+					else
+						clearInterval(scrollInterval);
+				}, 15);
+			});
+		}
 	}
 
 })();
