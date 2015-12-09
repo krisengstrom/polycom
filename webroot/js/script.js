bf8cdbee2fd44827536471eba72935f2f84f4de5
krisengstrom <kristofer.engstrom@gmail.com>
2015-12-09 11:51:38 +1100

    confirmation pages

---
+++ webroot/js/script.js
@@ -0,0 +1,16 @@
+(function() {
+
+	var buttons = document.querySelectorAll('button');
+	for (var i=0; i<buttons.length; i++) {
+		buttons[i].addEventListener('click', function() {
+			var scrollStep = -window.scrollY / (1000 / 15);
+			var scrollInterval = setInterval(function() {
+				if (window.scrollY != 0)
+					window.scrollBy(0, scrollStep);
+				else
+					clearInterval(scrollInterval);
+			}, 15);
+		});
+	}
+
+})();
